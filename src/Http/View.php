a04ff553d2089f8f96fa1b6be2ce4f65b57114de
yuribertulucci <yuribertulucci2004@gmail.com>
2025-12-17 10:36:32 -0300

    feat: add parent view support and improve placeholder replacement

--- src/Http/View.php
+++ src/Http/View.php
@@ -30,7 +30,7 @@ class View
 
     public function loadViewContent(string $view): void
     {
-        $this->viewPath = $this->viewsPath . str_replace('.', '/', $view) . '.php';
+        $this->viewPath = self::getViewPath($view);
 
         if (file_exists($this->viewPath)) {
             $this->content = file_get_contents($this->viewPath);
@@ -39,7 +39,25 @@ class View
 
     private function replacePlaceholders(): void
     {
+        preg_match('/@extends\s*\(\s*[\'"](.+)[\'"]\s*\)/', $this->content, $matches);
+        if (isset($matches[1])) {
+            $parentView = $matches[1];
+            if (self::exists($parentView)) {
+                $childContent = preg_replace('/@extends\s*\(\s*[\'"](.+)[\'"]\s*\)/', '', $this->content);
+                $parentContent = file_get_contents(self::getViewPath($parentView));
+                $this->content = str_replace(['{!! $slot !!}', '{{ $slot }}'], $childContent, $parentContent);
+            }
+        }
+
         $content = preg_replace('/{{\s*(.+)\s*}}/', '<?php echo encode($1); ?>', $this->content);
+
+        $content = preg_replace('/@if\s*\((.+)\)/', '<?php if ($1): ?>', $content);
+        $content = preg_replace('/@elseif\s*\((.+)\)/', '<?php elseif ($1): ?>', $content);
+        $content = preg_replace('/@else/', '<?php else: ?>', $content);
+        $content = preg_replace('/@endif/', '<?php endif; ?>', $content);
+
+        $content = preg_replace('/@include\s*\(\s*[\'"](.+)[\'"]\s*\)/', '<?php include app_path("resources/views/" . str_replace(".", "/", "$1") . ".php"); ?>', $content);
+
         $this->content = $content;
     }
 
@@ -47,5 +65,10 @@ class View
     {
         $viewPath = app_path('resources/views/' . str_replace('.', '/', $view) . '.php');
         return file_exists($viewPath);
+    }
+
+    public static function getViewPath(string $view): string
+    {
+        return app_path('resources/views/' . str_replace('.', '/', $view) . '.php');
     }
 }
