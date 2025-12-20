8f854c80512ded7e22aad37e6ac4b5b51ae9b720
yuribertulucci <yuribertulucci2004@gmail.com>
2025-12-20 12:27:46 -0300

    feat: add modular URL generation from SwApi URLs

--- src/DTO/BaseDTO.php
+++ src/DTO/BaseDTO.php
@@ -28,17 +28,26 @@ abstract class BaseDTO
         $url = env('SWAPI_URL', 'https://swapi.py4e.com/api/');
         array_walk_recursive($data, function (&$value, $key) use ($url) {
             if ($key === 'url') {
-                $value = str_replace($url, '/api/', $value);
-            }
-            if (is_string($value) && str_contains($value, $url)) {
-                $value = str_replace($url, '/api/', $value);
-            }
-            if ($key === 'next' || $key === 'previous' && $value !== null) {
+                $model = $this->extractModelFromUrl($value);
+                $value = route("api.$model.show", ['id' => $this->extractIdFromUrl($value)]);
+            } elseif ($key === 'next' || $key === 'previous' && $value !== null) {
                 $value = str_replace('&format=json', '', $value);
+            } elseif (is_string($value) && str_contains($value, $url) && is_string($key) && !empty($key)) {
+                $model = $key === 'homeworld' ? 'planets' : $key;
+                $value = route("api.$model.show", ['id' => $this->extractIdFromUrl($value)]);
+            } elseif (is_string($value) && str_contains($value, $url)) {
+                $model = $this->extractModelFromUrl($value);
+                $value = route("api.$model.show", ['id' => $this->extractIdFromUrl($value)]);
             }
         });
 
         return $data;
+    }
+
+    protected function extractModelFromUrl(string $url): string
+    {
+        $parts = explode('/', trim($url, '/'));
+        return $parts[count($parts) - 2] ?? '';
     }
 
     protected function extractIdFromUrl(string $url): ?int
